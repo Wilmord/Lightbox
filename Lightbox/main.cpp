29f98a4b0029ed9ffe77d2f49c84e8fe1a4dacbe
Brky <berkayelbir10@hotmail.com>
2023-12-20 16:22:39 +0100

    Adding first library/module

--- Lightbox/main.cpp
+++ Lightbox/main.cpp
@@ -1,24 +1,32 @@
-#include <cstdio>
-#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
 
-using namespace cv;
+import Display;
 
-int main(int argc, char** argv )
+
+int main(int argc, char* argv[])
 {
     if ( argc != 2 )
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage:  DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    Mat image;
-    image = imread( argv[1], IMREAD_COLOR );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
+
+    std::string current_exec_name = argv[0]; // Name of the current exec program
+    std::string imagePath;
+
+    if (argc > 1) {
+        imagePath = argv[1];
+        Display d;
+        d.display(imagePath);
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
+
+
+
     return 0;
 }
+
+//Bu maini wrap et Display image die sonra onu export module yap
+//Burada onu import edelim, daha simdiden kodu bolelm dependency sadece o module de kalsin
+//CLI yapabilirm, GUI ile ugrasilmaz
